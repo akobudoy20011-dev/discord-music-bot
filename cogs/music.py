1f1bfc276235238260ffc2399a6f065610e080f2
akobudoy20011-dev <akobudoy20011@gmail.com>
2026-08-27 00:38:07 +0800

    Update music.py

--- cogs/music.py
+++ cogs/music.py
@@ -60,6 +60,8 @@ YTDL_OPTIONS = {
     "no_warnings": True,
     "default_search": "auto",
     "source_address": "0.0.0.0",
+    "extractor_args": {"youtube": {"player_client": ["android"]}},
+
 }
 
 if YTDLP_COOKIES_FILE:
@@ -127,6 +129,7 @@ async def fetch_song_mp3(query):
         "quiet": True,
         "no_warnings": True,
         "noplaylist": True,
+        "extractor_args": {"youtube": {"player_client": ["android"]}},
     
     }
 
