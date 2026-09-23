e674f3454876b21683828fc247f78ad5b022c2a3
akobudoy20011-dev <akobudoy20011@gmail.com>
2026-09-24 07:00:31 +0800

    Add rpg/enemies.py

---
+++ rpg/enemies.py
@@ -0,0 +1,14 @@
+"""Starter ECLIPSE PvE enemy pool."""
+import random
+
+ENEMIES = {
+    "shadow_beast": {"name":"Shadow Beast","hp":90,"attack":13,"xp":55,"gold":35},
+    "ironfang_wolf": {"name":"Ironfang Wolf","hp":75,"attack":16,"xp":65,"gold":45},
+    "hollow_knight": {"name":"Hollow Knight","hp":125,"attack":18,"xp":95,"gold":70},
+    "ash_drake": {"name":"Ash Drake","hp":160,"attack":22,"xp":130,"gold":100},
+}
+
+def random_enemy():
+    key=random.choice(list(ENEMIES))
+    data=ENEMIES[key].copy(); data["id"]=key
+    return data
