20e16e41bfd09383b611235f1055b43d9870f6ce
akobudoy20011-dev <akobudoy20011@gmail.com>
2026-09-24 23:44:46 +0800

    refactor: centralize companion catalogue

--- cogs/companions.py
+++ cogs/companions.py
@@ -3,12 +3,7 @@
 import discord
 from discord.ext import commands
 
-COMPANIONS = {
-    "moonfox": {"name":"Moonfox","icon":"🦊","cost":5000,"description":"A lunar fox that sharpens agility and awareness.","bonus":{"agility":4,"magic":2}},
-    "emberwolf": {"name":"Emberwolf","icon":"🐺","cost":7500,"description":"A fire-born wolf that strengthens physical attacks.","bonus":{"strength":5,"max_hp":10}},
-    "starowl": {"name":"Starowl","icon":"🦉","cost":10000,"description":"A celestial owl that expands the mana pool.","bonus":{"magic":5,"max_mp":18}},
-    "voidcat": {"name":"Voidcat","icon":"🐈‍⬛","cost":15000,"description":"A creature of the Veil that balances every attribute.","bonus":{"strength":3,"defense":3,"magic":3,"agility":3}},
-}
+from rpg.companions import COMPANIONS
 
 class Companions(commands.Cog):
     def __init__(self, bot): self.bot=bot; self.db=bot.db
